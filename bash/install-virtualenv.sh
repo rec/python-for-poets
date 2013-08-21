cef49283e0d3e087adf8405068e122f99d747088
Tom Ritchford <tom@swirly.com>
2013-08-21 17:03:07 -0400

    Update script to get virtualmin 1.9

--- bash/install-virtualenv.sh
+++ bash/install-virtualenv.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 cd /tmp
-curl -O https://pypi.python.org/packages/source/v/virtualenv/virtualenv-1.11.tar.gz
-tar xvfz virtualenv-1.11.tar.gz
-cd virtualenv-1.11
+curl -O https://pypi.python.org/packages/source/v/virtualenv/virtualenv-1.9.tar.gz
+tar xvfz virtualenv-1.9.tar.gz
+cd virtualenv-1.9
 python setup.py install
+cd ..
+rm -Rf virtual*
